e82e737021125df7c8e0cd4e9579e233895b6ed4
unknown <D:\Mail>
2021-01-28 23:48:16 +0700

    Закончен урок.

--- config/init.php
+++ config/init.php
@@ -10,4 +10,11 @@ define("CASHE", ROOT . '/tmp/cadhe');
 define("CONFIG", ROOT . '/config');
 define("LAYOUT", 'default');
 
-echo $app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+$app_path = preg_replace("#[^/]+$#", "", $app_path);
+$app_path = str_replace("/public/", "", $app_path);
+
+define("PATH", $app_path);
+define("ALMIN", PATH . '/admin');
+
+require_once ROOT . '/vendor/autoload.php';
